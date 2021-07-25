e177bc7e353819ea18251950b5e570d2b13764b6
Deepak1jha <ddjha171@gmail.com>
2021-07-25 19:53:13 +0530

    final commit

--- src/main/java/com/springdatarest/SpringDataRestApplication.java
+++ src/main/java/com/springdatarest/SpringDataRestApplication.java
@@ -1,13 +1,20 @@
 package com.springdatarest;
 
+import com.springdatarest.model.Customer;
+import com.springdatarest.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringDataRestApplication {
+public class SpringDataRestApplication  {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringDataRestApplication.class, args);
-	}
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataRestApplication.class, args);
+    }
 
 }
--- src/main/java/com/springdatarest/repository/CustomerRepository.java
+++ src/main/java/com/springdatarest/repository/CustomerRepository.java
@@ -1,6 +1,11 @@
 package com.springdatarest.repository;
 
+import com.springdatarest.model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Repository;
 
-public interface CustomerRepository extends JpaRepository<CustomerRepository,Integer> {
+@Repository
+@RepositoryRestResource(path = "users",collectionResourceRel = "users")
+public interface CustomerRepository extends JpaRepository<Customer,Integer> {
 }
